e25aa5ddae1746192041220ca0e1744980c61f30
thibautDevWorld <thibaut.lartigue.pro@gmail.com>
2022-11-08 15:58:41 +0100

    fix some problems

--- src/index.js
+++ src/index.js
@@ -3,9 +3,9 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import { AuthContextProvider } from './context/AuthContext';
-import { disableReactDevTools } from '@fvilers/disable-react-devtools';
 
-if (process.env.NODE_ENV === 'production') disableReactDevTools()
+
+
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 
